85a0fca2ad93ebbe5012c506710d115e29a3dfd5
Levin Jian <jianzhirong@gmail.com>
2020-03-05 15:01:32 +0800

    change fit function

--- app/CurveFitting.cpp
+++ app/CurveFitting.cpp
@@ -47,6 +47,35 @@ public:
     double x_,y_;  // x 值， y 值为 _measurement
 };
 
+class CurveFittingEdge2: public Edge
+{
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    CurveFittingEdge2( double x, double y ): Edge(1,1, std::vector<std::string>{"abc"}) {
+        x_ = x;
+        y_ = y;
+    }
+    // 计算曲线模型误差
+    virtual void ComputeResidual() override
+    {
+        Vec3 abc = verticies_[0]->Parameters();  // 估计的参数
+        residual_(0) =  abc(0)*x_*x_ + abc(1)*x_ + abc(2)  - y_;  // 构建残差
+    }
+
+    // 计算残差对变量的雅克比
+    virtual void ComputeJacobians() override
+    {
+        Vec3 abc = verticies_[0]->Parameters();
+        Eigen::Matrix<double, 1, 3> jaco_abc;  // 误差为1维，状态量 3 个，所以是 1x3 的雅克比矩阵
+        jaco_abc << x_ * x_ , x_  , 1 ;
+        jacobians_[0] = jaco_abc;
+    }
+    /// 返回边的类型信息
+    virtual std::string TypeInfo() const override { return "CurveFittingEdge"; }
+public:
+    double x_,y_;  // x 值， y 值为 _measurement
+};
+
 int main()
 {
     double a=1.0, b=2.0, c=1.0;         // 真实参数值
@@ -71,11 +100,12 @@ int main()
         double x = i/100.;
         double n = noise(generator);
         // 观测 y
-        double y = std::exp( a*x*x + b*x + c ) + n;
-//        double y = std::exp( a*x*x + b*x + c );
+//        double y = std::exp( a*x*x + b*x + c ) + n;
+        double y = a*x*x + b*x + c;
 
         // 每个观测对应的残差函数
-        shared_ptr< CurveFittingEdge > edge(new CurveFittingEdge(x,y));
+//        shared_ptr< CurveFittingEdge > edge(new CurveFittingEdge(x,y));
+        shared_ptr< CurveFittingEdge2 > edge(new CurveFittingEdge2(x,y));
         std::vector<std::shared_ptr<Vertex>> edge_vertex;
         edge_vertex.push_back(vertex);
         edge->SetVertex(edge_vertex);
